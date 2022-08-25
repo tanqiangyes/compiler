e6f327d16d0dd6ce33b062d4f47c40529f1d2d47
tanqiangyes <826285820@qq.com>
2022-08-25 11:40:16 +0800

    init repo

---
+++ main.go
@@ -0,0 +1,12 @@
+// Package compiler
+/******************************************************************************
+ * Author         : jack
+ * Mail           : jack.tan@arsyun.com
+ * Last modified  : 2022/8/25$ 11:23$
+ * Description    : just for compiler main function.
+*******************************************************************************/
+package main
+
+func main() {
+
+}
